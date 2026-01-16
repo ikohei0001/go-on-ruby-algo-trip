3dab8677a48d991b662e23f549a576e985769801
ikohei0001 <labellavita_140@ezweb.ne.jp>
2026-01-16 21:23:25 +0900

    Add Rspec helper file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,12 +11,13 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
-#
+Dir[File.join(__dir__, "support/**.rb")].sort.each { |f| require f }
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  config.include MinHeapProperty
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
