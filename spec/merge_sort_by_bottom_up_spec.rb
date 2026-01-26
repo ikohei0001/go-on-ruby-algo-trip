88ea5068edf2b9bcc78dcc5656471da7bbd82631
kohei <labellavita_140@ezweb.ne.jp>
2026-01-26 19:50:27 +0900

    Add RSpec test file for merge_sort_by_bottom_up.rb

---
+++ spec/merge_sort_by_bottom_up_spec.rb
@@ -0,0 +1,7 @@
+require_relative "../lib/merge_sort_by_bottom_up.rb"
+
+RSpec.describe "#sort_array" do
+  it "sorts an array in ascending order" do
+    expect(sort_array([3, 5, 1, 6, 9, 4, 2, 8, 7])).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
+  end
+end
